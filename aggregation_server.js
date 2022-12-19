cba4addfd4337eba546d643ef108e6ea59887e2a
Artur Brzezina <arturbb11@o2.pl>
2022-12-19 23:15:29 +0100

    modify aggregator and add statistics for data

--- aggregation_server.js
+++ aggregation_server.js
@@ -18,40 +18,121 @@ app.set('views', './views');
 app.use(express.static(__dirname + '/public'));
 
 // store data in array
-const storage = {
-
-}
+const storage = { };
 
 app.get('/', (req, res) => {
     res.send(storage);
-})
+});
+
+app.get('/agg/:srcName/show/stats', (req, res) => {
+    const sourceName = req.params.srcName;
+    const lastNumber = req.query.lastNumber;
+    const filterAttribute = req.query.getAttribute;
+    try {
+        const lastMs = [storage[sourceName][`last${lastNumber}`].slice(-1)[0].slice(0, -1)];
+    const shareDataTimestamp = 
+        storage[sourceName][`last${lastNumber}`].slice(-1)[0].slice(-1)[0];
+    const filterData = [];
+    for (let measure of lastMs[0]) {
+        filterData.push(Number(measure[filterAttribute]));
+    }
+    const max = Math.max(...filterData);
+    const min = Math.min(...filterData);
+    const mean = filterData.reduce((acc, curr) => acc + curr, 0) / filterData.length;
+    res.render('show_stats', {
+        rows: lastNumber,
+        shareData: filterData,
+        attribute: filterAttribute,
+        timestamp: shareDataTimestamp,
+        max: max,
+        min: min,
+        average: mean
+    });
+    } catch {
+        res.render('show_stats');
+    }
+    
+});
 
 app.get('/agg/:srcName/show', (req, res) => {
     const sourceName = req.params.srcName;
     const rowsNumber = req.query.getData;
     console.log(sourceName, rowsNumber);
-    console.log(Object.keys(storage[sourceName][0]));
-    const shareData = storage[sourceName].slice(-rowsNumber);
-    res.render('show_stored_data', {
-        shareData: shareData,
-        rows: rowsNumber,
-        labels: Object.keys(storage[sourceName][0]),
-        srcName: sourceName
-    });
-})
+    console.log(storage[sourceName]);
+    
+    try {
+        const shareData = 
+            [storage[sourceName][`last${rowsNumber}`].slice(-1)[0].slice(0, -1)];
+        const shareDataTimestamp = 
+            storage[sourceName][`last${rowsNumber}`].slice(-1)[0].slice(-1)[0];
+        console.log('awaiting for data');
+        console.log(shareDataTimestamp);
+        // console.log('original', shareData);
+        res.render('show_stored_data', {
+            shareData: shareData,
+            rows: rowsNumber,
+            srcName: sourceName,
+            attributes: storage[sourceName]['attributes'],
+            package: storage[sourceName][`last${rowsNumber}`],
+            timestamp: shareDataTimestamp
+        });
+    } catch {
+        res.render('show_stored_data', {
+            shareData: [],
+            rows: rowsNumber,
+            srcName: sourceName,
+            attributes: storage[sourceName]['attributes'],
+            package: storage[sourceName][`last${rowsNumber}`]
+        });
+    }
+
+    
+});
 
 app.post('/agg/:srcName/measurment', (req, res) => {
     const row = req.body;
     const sourceName = req.params.srcName;
-    console.log(row)
+    // console.log(row);
+
     if (!storage.hasOwnProperty(sourceName)) {
-        storage[sourceName] = [];
-        storage[sourceName].push(row);
-    } else {
-        storage[sourceName].push(row);
+        storage[sourceName] = {};
+        storage[sourceName]['last5'] = [];
+        storage[sourceName]['last10'] = [];
+        storage[sourceName]['last15'] = [];
+        storage[sourceName]['last20'] = [];
+
+        storage[sourceName]['temp5'] = [];
+        storage[sourceName]['temp10'] = [];
+        storage[sourceName]['temp15'] = [];
+        storage[sourceName]['temp20'] = [];
+
+        storage[sourceName]['attributes'] = Object.keys(row);
     }
-    res.send('ok')
-})
 
+    const collectAndStoreLast = lastStore => {
+        lastStore = parseInt(lastStore);
+        if (storage[sourceName][`temp${lastStore}`].length < lastStore) {
+            storage[sourceName][`temp${lastStore}`].push(row);
+        } else {
+            const curr = new Date();
+            const timestamp = 
+            `${curr.getDate()}/${curr.getMonth()}/${curr.getFullYear()} \
+            ${curr.getHours()}:${('0'+curr.getMinutes()).slice(-2)}::${('0'+curr.getSeconds()).slice(-2)}`;
 
-app.listen(3004, 'localhost')
+            storage[sourceName][`temp${lastStore}`].push(timestamp);
+            const pushPackage = storage[sourceName][`temp${lastStore}`];
+            storage[sourceName][`last${lastStore}`].push(pushPackage);
+            
+            storage[sourceName][`temp${lastStore}`] = [];
+        }
+    }
+
+    collectAndStoreLast(5);
+    collectAndStoreLast(10);
+    collectAndStoreLast(15);
+    collectAndStoreLast(20);
+
+    res.send('ok');
+});
+
+app.listen(3004, 'localhost');
